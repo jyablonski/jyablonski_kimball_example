6c41b45d57d7d26dab267c29cd2434b20d9f5dd9
jacob <jyablonski9@gmail.com>
2023-09-17 18:09:58 -0700

    Add fake invoice payment data (#2)
    * add
    
    * add
    
    * add notes
    
    * add
    
    * woop

--- docker/postgres_bootstrap.sql
+++ docker/postgres_bootstrap.sql
@@ -2,6 +2,7 @@ CREATE SCHEMA source;
 CREATE SCHEMA dbt_stg;
 CREATE SCHEMA dbt_prod;
 
+-- default directory for every statement in this bootstrap
 SET search_path TO source;
 
 -- Create the sales data tables
@@ -30,6 +31,28 @@ CREATE TABLE sales (
     created_at timestamp default current_timestamp
 );
 
+CREATE TABLE invoices (
+    invoice_id serial PRIMARY KEY,
+    customer_id integer,
+    sale_id integer,
+    is_voided boolean default FALSE,
+    created_at timestamp default current_timestamp
+
+);
+
+-- if Cash then leave `payment_type_info` NULL
+-- if Credit Card then input last 4 digits
+-- if Gift Card then input Full Gift Card number
+CREATE TYPE payment_enum AS ENUM ('Cash', 'Credit Card', 'Gift Card');
+CREATE TABLE payments (
+    payment_id serial PRIMARY KEY,
+    amount decimal(10, 2),
+    payment_type payment_enum,
+    payment_type_info text,
+    invoice_id integer,
+    created_at timestamp default current_timestamp
+);
+
 -- this basically acts as an audit table - will have multiple rows for each change per customer id / integration
 CREATE TYPE integration_enum AS ENUM ('Mailchimp', 'Salesforce', 'Hubspot');
 CREATE TABLE integrations (
@@ -37,6 +60,12 @@ CREATE TABLE integrations (
     customer_id integer,
     integration_type integration_enum,
     is_active integer,
+    created_at timestamp default current_timestamp
+);
+
+CREATE TABLE orders (
+    order_id serial PRIMARY KEY,
+    external_data json not null,
     created_at timestamp default current_timestamp
 );
 
@@ -76,3 +105,20 @@ VALUES
     (2, 'Salesforce', 1, current_timestamp + interval '14 days'),
     (3, 'Hubspot', 0, current_timestamp + interval '14 days'),
     (1, 'Salesforce', 0, current_timestamp + interval '21 days');
+
+INSERT INTO invoices (customer_id, sale_id)
+VALUES 
+    (1, 1),
+    (2, 2),
+    (1, 3);
+
+INSERT INTO payments (amount, payment_type, payment_type_info, invoice_id)
+VALUES 
+    (32.97, 'Cash', NULL, 1),
+    (39.98, 'Credit Card', '4436', 2),
+    (3.96, 'Gift Card', 'G42423241', 3),
+    (60, 'Cash', NULL, 3);
+
+INSERT INTO orders (external_data)
+VALUES
+    ('{"id": 1, "source": {"address": "123 Wells Way", "store": "Walgreens", "state": "IL", "zip_code": 60601, "transaction_timestamp": "2023-09-17 20:00:00.000000"}, "sale_id": 4}');
--- jacobs_notes.md
+++ jacobs_notes.md
@@ -1,3 +1,27 @@
+# Why Kimball Modeling
+Goal is to make consuming & analyzing data easier.  Source Data in Application Databases may be setup to be efficient for Applications + adhere to traditional OLTP principles, but it's probably not optimized for Reporting.
+- Improves the structure, quality, and ease-of-use of the data to optimize it for downstream uses like BI & Reporting.
+- Introduces a common, standardized way of modeling data.  New people can come in and be familiar with what Fact, Dim, and SCD2 tables all do.
+- Helps organize the data landscape when you have tons of different data sources flowing in.
+
+Kimball is not 100% necessary; at the end of the day as long as you can efficiently provide business value with data to business users and other stakeholders then it doesn't matter what the underlying methodology being used is.
+## Facts
+Facts represent quantitative & measurable pieces of data about business events or processes.
+- Facts can be linked to dimensions through Foreign Keys to create relationships between the two.
+- Typically Fact Tables have lots of rows with few columns
+- Examples include Sales or Order data
+
+## Dimensions
+Dimensions are business descriptive attributes related to the contents in the Fact Tables.
+- Describes the who, what, where, when, and why.
+- Typically don't change very often
+- Typically Dimensions Tables have more columns and fewer rows than Fact Tables
+- Examples include Product, Customer, Geography based attributes
+
+Cons
+- More up-front work needed to get set everything up
+
+## Local Dev
 `asdf local python 3.11.5`
 `poetry install`
 `dbt init` type in your project name and then cut out all of the files it creates and paste them in root directory
