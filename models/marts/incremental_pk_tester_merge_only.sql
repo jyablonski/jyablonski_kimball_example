b290a0c5d1092b3d101708751d9702fb8a446a78
jyablonski9 <jyablonski9@gmail.com>
2023-12-07 10:04:43 -0600

    add

--- models/marts/incremental_pk_tester_merge_only.sql
+++ models/marts/incremental_pk_tester_merge_only.sql
@@ -2,10 +2,12 @@
     config(
         materialized='incremental',
         unique_key="sale_id",
-        post_hook="delete from {{ this }} where sale_id not in (select sale_id from {{ source('application_db', 'sales') }} )"
+        post_hook="delete from {{ this }} where sale_id not in (select sale_id from {{ source('application_db', 'sales') }} )",
+        merge_update_columns = ['customer_id', 'product_id', 'total_amount', 'created_at', 'modified_at'],
     )
 }}
 -- post hook - after running, delete any record in this table that has been deleted in the source table
+-- merge updsate columns only works for snowflake i guess, not postgres
 
 select *
 
@@ -15,6 +17,6 @@ from {{ source('application_db', 'sales') }}
 
     -- this filter will only be applied on an incremental run
     -- (uses > to include records whose timestamp occurred since the last run of this model)
-    where sale_id not in (select sale_id from {{ this }})
+    where modified_at > (select max(modified_at) from {{ this }})
 
 {% endif %}
