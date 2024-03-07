6269994cb2e5b2f63ea54f59d8958838c10f218e
jyablonski9 <jyablonski9@gmail.com>
2024-03-07 17:14:46 -0600

    fv

--- models/marts/marketing/customers_to_email.sql
+++ models/marts/marketing/customers_to_email.sql
@@ -1,7 +1,7 @@
 -- customers who made a purchase in the last year, but not in the last 30 days 
 -- that we could potentially market to
 
-with customer_orders as (
+with past_customer_orders as (
     select
         orders_detailed.order_detail_id,
         customers.customer_name,
@@ -11,8 +11,10 @@ with customer_orders as (
     from {{ ref('orders_detailed') }}
         inner join {{ ref('customers') }} on orders_detailed.customer_id = customers.customer_id
         inner join {{ ref('products') }} on orders_detailed.product_id = products.product_id
-
+    where
+        orders_detailed.order_detail_created_at < current_date - interval '30 days'
+        and orders_detailed.order_detail_created_at >= current_date - interval '1 year'
 )
 
 select *
-from customer_orders
+from past_customer_orders
